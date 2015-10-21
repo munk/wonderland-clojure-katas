3805a18ef1a0c0588ef9bf152ade52be87e21899
john.downs <john.downs@yodle.com>
2015-10-21 15:14:49 -0400

    game of war

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -8,6 +8,35 @@
         rank ranks]
     [suit rank]))
 
-(defn play-round [player1-card player2-card])
+(defn rank-val [card]
+  (let [[_ rank] card]
+    (.indexOf ranks rank)))
 
-(defn play-game [player1-cards player2-cards])
+(defn suit-val [card]
+  (let [[suit _] card]
+    (.indexOf suits suit)))
+
+(defn card-val [card]
+  [(rank-val card) (suit-val card)])
+
+(defn play-round [player1-card player2-card]
+  (println "Player One: " player1-card)
+  (println "Player Two: " player2-card)
+  (let [[p1-suit p1-rank] (card-val player1-card)
+        [p2-suit p2-rank] (card-val player2-card)
+        prize (shuffle [player1-card player2-card])]
+    (cond (> p1-rank p2-rank) [prize []]
+          (> p2-rank p1-rank) [[] prize]
+          (> p1-suit p2-suit) [prize []]
+          true [[] prize])))
+
+(defn play-game [player1-cards player2-cards]
+  (cond (empty? player1-cards) :player-2
+        (empty? player2-cards) :player-1
+        true (let [c1 (first player1-cards)
+                   c2 (first player2-cards)
+                   c1' (rest player1-cards)
+                   c2' (rest player2-cards)
+                   [p1 p2] (play-round c1 c2)]
+               (println player1-cards player2-cards)
+               (recur (into c1' [p1]) (into c2' [c2])))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,18 +2,37 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
+(defn wins? [result]
+  (let [[p1 p2] result]
+    (if (empty? p1)
+      :player-2
+      :player-1)))
 
-;; fill in  tests for your game
+;; Fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= :player-2
+           (wins? (play-round [:spade 2] [:spade :ace])))))
+  (testing "queens are higher rank than jacks"
+    (is (= :player-1
+           (wins? (play-round [:foo :queen] [:foo :jack])))))
+  (testing "kings are higher rank than queens"
+    (is (= :player-2
+           (wins? (play-round [:foo :queen] [:foo :king])))))
+  (testing "aces are higher rank than kings"
+    (is (= :player-2
+           (wins? (play-round [:foo :king] [:foo :ace])))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= :player-1
+           (wins? (play-round [:club 2] [:spade 2])))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= :player-2
+           (wins? (play-round [:club :ace] [:diamond :ace])))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= :player-1
+           (wins? (play-round [:heart :ace] [:diamond :ace]))))))
 
 (deftest test-play-game
-  (testing "the player loses when they run out of cards"))
-
+  (testing "the player loses when they run out of cards"
+    (is (= :player-1
+           (play-game [[:heart :ace]] [[:diamond :ace]])))))
